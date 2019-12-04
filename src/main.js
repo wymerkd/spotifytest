671f6ca133f31535369a4593438d16b1fdf8b990
Eliza Sohn <eliza.sohn@gmail.com>
2019-12-03 22:50:59 -0800

    change api url to target recommendations, append response with artist name, track, album title and image

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ $("#apiButton").on("click",function(){
    console.log(window.location)
    var uriHash= window.location.hash //holds the access token
    var accessToken = uriHash.replace('#access_token=', '')
-   var apiURL= "https://api.spotify.com/v1/me/"
+   var apiURL= "https://api.spotify.com/v1/recommendations?limit=10&market=US&seed_genres=country&target_valence=0.4"
   $.ajax({
 
       url: apiURL,
@@ -15,8 +15,9 @@ $("#apiButton").on("click",function(){
      },
       success: function(response){
           console.log(response);
-          $("h2").append("<h2>"+response.display_name+", "+"Followers:"+ " "+response.followers.total+ " " +", Lives in"+" "+response.country+"</h2>")
-          $("img").attr("src", response.images[0].url)
+          $("h2").append("<h2>"+response.tracks[0].album.artists[0].name+", "+ " "+response.tracks[0].name+ " on the album " +response.tracks[0].album.name+ " " +response.tracks[0].external_urls.spotify+"</h2>")
+          $("img").attr("src", response.tracks[0].album.images[1].url);
+          
       },
 
       error: function(r){
