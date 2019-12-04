ef7c44d3e2a32785f77a6b34b133868290e63e51
Eliza Sohn <eliza.sohn@gmail.com>
2019-12-03 21:34:36 -0800

    add existing code to clean template without adding previous dependencies

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import $ from 'jquery';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$("#apiButton").on("click",function(){
+   console.log(window.location)
+   var uriHash= window.location.hash //holds the access token
+   var accessToken = uriHash.replace('#access_token=', '')
+   var apiURL= "https://api.spotify.com/v1/me/"
+  $.ajax({
+
+      url: apiURL,
+      headers: {
+       'Authorization': 'Bearer ' + accessToken
+     },
+      success: function(response){
+          console.log(response);
+          $("h2").append("<h2>"+response.display_name+", "+"Followers:"+ " "+response.followers.total+ " " +", Lives in"+" "+response.country+"</h2>")
+          $("img").attr("src", response.images[0].url)
+      },
+
+      error: function(r){
+        console.log(r);
+  }
+  })
+})
